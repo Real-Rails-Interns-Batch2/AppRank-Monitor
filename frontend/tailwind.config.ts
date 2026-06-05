1df9a4c93d597f76523153085eecf4c51091b664
jaliba-sherin96 <jalibasherin@zohomail.in>
2026-06-05 11:07:17 +0530

    Update and rename tailwind.config.js to tailwind.config.ts

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
